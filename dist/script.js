80dd44de82b48acd322971f174af8ba7a6c158c2
Yuuki175 <realouise175@gmail.com>
2023-03-21 00:04:06 +0800

    final--second commmit

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,25 @@
+const toggleMenuDev = document.querySelector(".toggle__menuDev");
+const headerDev = document.querySelector(".headerDev__tabs2 ul");
+toggleMenuDev.addEventListener("click", () => {
+  toggleMenuDev.classList.toggle("open");
+  headerDev.classList.toggle("open");
+});
+const kamote = document.querySelectorAll(".footer__items h4");
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener("click", () => {
+        kamoteq.nextElementSibling.classList.toggle("open");
+        kamoteq.querySelector("i").classList.toggle("open");
+    })
+})
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav ul");
+const arjay = document.querySelector(".arjay");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
-  headerNav.style=("transition: .5s ease")
+  headerNav.style=("transition: .5s ease");
+  arjay.classList.toggle("show");
 });
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,27 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      dropShadow: {
+        'custom': '0 0 5px rgba(0, 0, 0, 0.3)',
+        'custom1': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
       colors: {
         blue:'#1d75de',
+        blue2:'#0d72ea',
         dark:'#000000',
         light:'#FFFFFF',
         green:'#1ed760',
-      }
+        darkgreen:'#056952',
+        fontlime:'#c5f0c9',
+        dark1:'#191414',
+        dark2:'#222326',
+        dark3:'#efefef',
+        fontgray:'#535353',
+      },
     },
-    // container:{
-    //   center: true,
-    //   padding:'15px',
-    //   max-with
-    // },  
   },
   plugins: [],
 }
